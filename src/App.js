a4f3028e2c860f0be2ca4f71e6ae0984212ed8e5
abegpatel <abegpatel@gmail.com>
2021-06-14 15:24:56 +0530

    adding port

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://blooming-atoll-64517.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
